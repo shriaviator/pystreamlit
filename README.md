fb22e1c1ea0ce2b0ebd1b261244dc621db88508b
shriaviator <32628818+shriaviator@users.noreply.github.com>
2021-02-17 11:22:27 +0530

    tasks removed from streamlit to master performance improv

--- README.md
+++ README.md
@@ -6,3 +6,16 @@
 
 - [Tidy Data Principles Used for Cleaning data](http://vita.had.co.nz/papers/tidy-data.pdf)
 - StreamLit provides visualisation 
+- Data is saved in python pickle for faster Loading
+
+## To do list 
+
+ - [ ] make pickle object with multiple variables 
+  
+ - [x] Airport List Sort
+ - [ ] When dropping value based in the master dataset are there any edge cases 
+ - [ ] "Text Comment"]=="A124" is a edge case  Vimp
+ - [ ] 6E 497 STV HYD is not appearing T/s required
+      - remove_duplicate_flights(dataframe)
+      - dataframe = dataframe.drop_duplicates(subset=['Flt Desg'])
+ - [ ] 
--- streamlit.py
+++ streamlit.py
@@ -11,56 +11,12 @@ import pickle
 import re
 import altair as alt
 st.set_page_config(page_title=" Jebah", page_icon="favicon.ico", layout="wide")
-alpha = pickle.load(open("gsheet_pickle.p", "rb"))
-# dropping columns for final assymbly
-alpha = alpha.drop(['moddeptime', 'mod_arr_time', 'Freq',
-                    'Modeffdate', 'moddisdate', 'actual_days'], axis=1)
-alpha = pd.melt(alpha, id_vars=['Dept Arp', 'engg_arr', 'engg_dep', 'deptype', 'Arvl Arp', 'Flt Desg',
-                                'Text Comment', 'Block Time'], value_vars=['D1', 'A1', 'D2', 'A2', 'D3', 'A3', 'D4',
-                                                                           'A4', 'D5', 'A5', 'D6', 'A6', 'D7', 'A7', 'D8', 'A8', 'D9', 'A9', 'D10',
-                                                                           'A10', 'D11', 'A11', 'D12', 'A12', 'D13', 'A13', 'D14', 'A14', 'D15',
-                                                                           'A15', 'D16', 'A16', 'D17', 'A17', 'D18', 'A18', 'D19', 'A19', 'D20',
-                                                                           'A20', 'D21', 'A21', 'D22', 'A22', 'D23', 'A23', 'D24', 'A24', 'D25',
-                                                                           'A25', 'D26', 'A26', 'D27', 'A27', 'D28', 'A28', 'D29', 'A29', 'D30',
-                                                                           'A30', 'D31', 'A31'])
+streamlit_pickle = pickle.load(open("gsheet_pickle.p", "rb"))
 
-filter_my_list_ray = [
-    "D1",
-    "D2",
-    "D3",
-    "D4",
-    "D5",
-    "D6",
-    "D7",
-    "D8",
-    "D9",
-    "D10",
-    "D11",
-    "D12",
-    "D13",
-    "D14",
-    "D15",
-    "D16",
-    "D17",
-    "D18",
-    "D19",
-    "D20",
-    "D21",
-    "D22",
-    "D23",
-    "D24",
-    "D25",
-    "D26",
-    "D27",
-    "D28",
-    "D29",
-    "D30",
-    "D31"
-]
-
+alpha = streamlit_pickle[0]
+filter_my_list_ray = streamlit_pickle[1]
+dep_airport_list = streamlit_pickle[2]
 dep_count_list = []
-
-dep_airport_list = list(alpha['Dept Arp'].unique())
 
 
 option = st.sidebar.selectbox(
@@ -78,10 +34,18 @@ st.markdown(title_style, unsafe_allow_html=True)
 'Airport selected:', option
 
 for india in filter_my_list_ray:
-    test_df = alpha.loc[(alpha["Dept Arp"] == option) &
-                        (alpha["variable"] == india)]
-    test_df = test_df[test_df.value != False].sort_values(by="value")
-    dep_count_list.append(test_df["value"].count())
+    if (option == "All_Stations"):
+        my_df = alpha.loc[
+            (alpha["variable"] == india)]
+
+        my_df = my_df[my_df.value != False].sort_values(by="value")
+        dep_count_list.append(my_df["value"].count())
+
+    else:
+        test_df = alpha.loc[(alpha["Dept Arp"] == option) &
+                            (alpha["variable"] == india)]
+        test_df = test_df[test_df.value != False].sort_values(by="value")
+        dep_count_list.append(test_df["value"].count())
 
 
 chart_data = pd.DataFrame({
